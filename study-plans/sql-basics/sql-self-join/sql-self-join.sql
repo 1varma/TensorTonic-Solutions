3e9f608fd7b6c1507d2b9739d9f5101bbe4648a1
Ashish Varma Juttu <varmaashish53@gmail.com>
2026-05-23 03:41:23 -0400

    Add solution for sql-self-join

---
+++ study-plans/sql-basics/sql-self-join/sql-self-join.sql
@@ -0,0 +1,7 @@
+SELECT 
+    u.username,
+    COALESCE(r.username, 'organic') AS referrer_name
+FROM user_referrals AS u
+LEFT JOIN user_referrals AS r 
+    ON u.referred_by = r.id
+ORDER BY u.username ASC;
