86d5491e9b5c52532871f411db1c78841bee76c9
Ashish Varma Juttu <varmaashish53@gmail.com>
2026-05-29 20:27:57 -0400

    Add solution for numpy-filter-extract

---
+++ study-plans/numpy-basics/numpy-filter-extract/numpy-filter-extract.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def filter_and_extract(data, row_start, row_stop, threshold):
+    """
+    Returns: 1D ndarray of float64
+    """
+    arr = np.array(data[row_start:row_stop], dtype=np.float64)
+
+    return arr[arr > threshold]
