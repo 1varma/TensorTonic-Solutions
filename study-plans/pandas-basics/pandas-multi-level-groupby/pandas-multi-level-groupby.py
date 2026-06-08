5f69d0aa6d0d8f5bd222140ffbfb4434490d6850
Ashish Varma Juttu <varmaashish53@gmail.com>
2026-06-08 03:21:22 -0400

    Solve pandas-multi-level-groupby

---
+++ study-plans/pandas-basics/pandas-multi-level-groupby/pandas-multi-level-groupby.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+def multi_groupby(data, group_cols, value_col, aggfunc):
+    """
+    Returns: dict of lists (flat table with group columns + value column)
+    """
+    df = pd.DataFrame(data)
+
+    grouped= df.groupby(group_cols)[value_col].agg(aggfunc).reset_index()
+
+    return grouped.to_dict('list')
